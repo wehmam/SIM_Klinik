101dcbb61be99ec6c467031fc200a07d14248b10
wehmam <readytosurff@gmail.com>
2020-07-07 09:48:39 +0700

    mengganti redirect ketika sudah login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -31,7 +31,7 @@ class RegisterController extends Controller
      * @var string
      */
     
-     protected $redirectTo = RouteServiceProvider::HOME;
+     protected $redirectTo = 'admin/pendaftaran';
 
     /**
      * Create a new controller instance.
